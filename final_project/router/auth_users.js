961d442bc6cdf8018f601bc8feaa728b66c5af83
Alexis <alexis@secmotic.com>
2024-07-23 07:56:20 -0400

    final project

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,8 +2,15 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+let booksArray = Object.values(books);
 
-let users = [];
+let users = [
+    {
+        "username":"John Doe",
+        "password":123456789
+    }
+    
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
@@ -15,14 +22,61 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.body;
+    if (!username) {
+        return res.status(404).json({ message: "Body Empty" });
+    }
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: user
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token in session
+        req.session.authorization = {
+            accessToken
+        }
+         res.status(200).json({ message: 'User successfully logged in' });
+    } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+    }  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Extract email parameter and find users with matching email
+    const isbn = req.params.isbn;
+    let filtered_books = booksArray.filter((book) => book.isbn === isbn);
+    if (filtered_books.length > 0) {
+        // Select the first matching user and update attributes if provided
+        let filtered_book = filtered_books[0];
+        
+         // Extract and update DOB if provided
+        
+        let review = req.query.review;    
+        if (review) {
+            filtered_book.review = review;
+        }
+        
+        // Replace old user entry with updated user
+        booksArray = booksArray.filter((book) => book.isbn != isbn);
+        booksArray.push(filtered_book);
+        
+        // Send success message indicating the user has been updated
+        res.send(`Review of book with ISBN: ${isbn} updated.`);
+    } else {
+        // Send error message if no user found
+        res.send(`Unable to find book! ${filtered_books}`);
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const remainingBooks = booksArray.filter((book) => book.isbn != isbn);
+    booksArray = remainingBooks;
+
+    res.send(`Review of book with ISBN: ${isbn} deleted.`);
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,99 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const booksArray = Object.values(books);
+const axios = require('axios');
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    users.push({
+        "username": req.query.username,
+        "password": req.query.password,
+    });
+    res.send("The user " + req.query.username + " has been added!");
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify({books}, null, 4));
 });
+
+// Get the book list available in the shop
+/*public_users.get('/', async function (req, res) {
+    try {
+        const response = await axios.get('https://info12345283-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/');
+        const books = response.data;
+        res.send(JSON.stringify({ books }, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener los datos');
+    }
+});*/
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let filtered_books = booksArray.filter((book) => book.isbn === isbn);
+    res.send(JSON.stringify({filtered_books}, null, 4));
  });
+
+ /*public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+    try {
+        const response = await axios.get(`https://info12345283-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn?isbn=${isbn}`);
+        const booksArray = response.data;
+        let filtered_books = booksArray.filter((book) => book.isbn === isbn);
+        res.send(JSON.stringify({ filtered_books }, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener los datos');
+    }
+});*/
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let filtered_books = booksArray.filter((book) => book.author === author);
+    res.send(JSON.stringify({filtered_books}, null, 4));
 });
+
+/*public_users.get('/author/:author',async function (req, res) {
+    const author = req.params.author;
+    try {
+        const response = await axios.get(`https://info12345283-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author?author=${author}`);
+        const booksArray = response.data;
+        let filtered_books = booksArray.filter((book) => book.author === author);
+        res.send(JSON.stringify({ filtered_books }, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener los datos');
+    }
+});*/
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let filtered_books = booksArray.filter((book) => book.title === title);
+  res.send(JSON.stringify({filtered_books}, null, 4));
 });
+
+/*public_users.get('/title/:title',async function (req, res) {
+    const title = req.params.title;
+    try {
+        const response = await axios.get(`https://info12345283-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title?title=${title}`);
+        const booksArray = response.data;
+        let filtered_books = booksArray.filter((book) => book.title === title);
+        res.send(JSON.stringify({ filtered_books }, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener los datos');
+    }
+});*/
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const review = req.params.isbn;
+    let filtered_books = booksArray.filter((book) => book.review === review);
+    res.send(JSON.stringify({filtered_books}, null, 4));
 });
 
 module.exports.general = public_users;
